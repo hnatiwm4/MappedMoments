392729251df827d9e71d6611738b0a0cd6a279f7
Michael Hnatiw <michael@Pi.(none)>
2014-04-13 12:13:31 -0600

    Initial Commit: Start of Mapped Moments Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MappedMoments::Application.config.secret_key_base = 'c46d19799e6436ae2e1791b10b8d72e4d63f755071cb7e224850e456f07d22f0510c17768bf9d885fe7b1bdee6860b33af20645f8433d5da82a9f10bb1b8062e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MappedMoments::Application.config.session_store :cookie_store, key: '_MappedMoments_session'
